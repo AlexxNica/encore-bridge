c3966072727d7d446e2b56e247da24ce6d59853e
Julian Bonilla <julian.bonilla@rackspace.com>
2018-01-10 15:41:16 -0500

    Update to rxUnauthorizedInterceptor

--- dist/encore-bridge.js
+++ dist/encore-bridge.js
@@ -5,6 +5,53 @@ angular.module('encore.ui.utilities', []);
 angular.module('encore.ui.utilities')
 /**
  * @ngdoc service
+ * @name utilities.service:rxUnauthorizedInterceptor
+ * @description
+ * Simple injector which will intercept HTTP responses. If a HTTP 401 response error code is returned,
+ * the ui redirects to `/login`.
+ *
+ * @requires $q
+ * @requires @window
+ * @requires utilities.service:rxSession
+ *
+ * @example
+ * <pre>
+ * angular.module('encoreApp', ['encore.ui'])
+ *     .config(function ($httpProvider) {
+ *         $httpProvider.interceptors.push('rxUnauthorizedInterceptor');
+ *     });
+ * </pre>
+ */
+.factory('rxUnauthorizedInterceptor', ["$q", "$window", "rxSession", function ($q, $window, rxSession) {
+    var svc = {
+        redirectPath: function () {
+            // This brings in the entire relative URI (including the path
+            // specified in a <base /> tag), along with query params as a
+            // string.
+            // e.g https://www.google.com/search?q=woody+wood+pecker
+            // window.location.pathname = /search?q=woody+wood+pecker
+            return $window.location.pathname;
+        },
+        redirect: function (loginPath) {
+            loginPath = loginPath ? loginPath : '/login?redirect=';
+            $window.location = loginPath + encodeURIComponent(svc.redirectPath());
+        },
+        responseError: function (response) {
+            if (response.status === 401) {
+                rxSession.logout(); // Logs out user by removing token
+                svc.redirect();
+            }
+
+            return $q.reject(response);
+        }
+    };
+
+    return svc;
+}]);
+
+angular.module('encore.ui.utilities')
+/**
+ * @ngdoc service
  * @name utilities.service:rxTimePickerUtil
  *
  * @description
