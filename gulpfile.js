df027f24f17e15e63133aa46ef233541b167f07b
Julian Bonilla <julian.bonilla@rackspace.com>
2018-01-17 15:32:16 -0500

    Remove top level from less path

--- gulpfile.js
+++ gulpfile.js
@@ -74,7 +74,7 @@ gulp.task('build:styles', function () {
     // TODO: remove flexbox layout (fix rxForm)
     '@import "encore-ui/src/elements/FlexboxGrid/styles/layout.less";'
   ].join('\n')))
-  .pipe(less({ paths: ['./', './src', './src/styles', './encore-ui/src/styles', './node_modules/normalize.css'] }))
+  .pipe(less({ paths: ['./', './src/styles', './encore-ui/src/styles', './node_modules/normalize.css'] }))
   .pipe(gulp.dest('./demo/'));
 });
 
